ecd23bd0d35c4585e31b663598cb9614f6ad644c
Creol1 <gortunov93@gmail.com>
2024-01-13 16:49:22 +0300

    OrderList

--- src/modules/orders-list/ui/index.tsx
+++ src/modules/orders-list/ui/index.tsx
@@ -1,18 +1,12 @@
-import { Contract } from "ethers";
+import { Contract, ethers } from "ethers";
 import { useEffect, useState } from "react";
 import walletProvider from "../../../abi/wallet-provider";
 import * as stylex from "@stylexjs/stylex";
-import { abi, tokenAbi } from "../../../abi/abi";
-import {
-  CONTRACT_ADDRESS,
-  // TOKEN1_ADDRESS,
-  // TOKEN2_ADDRESS,
-} from "../../../abi/addresses";
-// import getPrice from "../../../helpers/get-price";
+import { abi, managerAbi } from "../../../abi/abi";
+import { CONTRACT_ADDRESS, INONFUNGIBLE_POSITION_MANAGER } from "../../../abi/addresses";
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import defaultProvider from "../../../abi/default-provider";
-import { useStores } from "../../../app/root-store-context";
+import { Button } from "@mui/material";
 
 const styles = stylex.create({
   wrapper: {
@@ -29,90 +23,137 @@ const styles = stylex.create({
   },
 });
 
-interface Order {
-  0: string;
-  1: number;
-  2: string;
-  3: string;
+interface PositionInfo {
+  token0: number;
+  token1: number;
+  tickLower: number;
+  tickUpper: number;
+  liquidity: BigInt;
+  fee: number;
 }
 
-type Orders = Order[];
-
 const OrdersList = () => {
-  const [orders, setOrders] = useState<Orders>([]);
-  const [expanded, setExpanded] = useState<string | false>(false);
-
-  const { user } = useStores();
+  const [position, setPosition] = useState<any[]>([]);
+  const [iD, setId] = useState<any[]>([]);
 
   useEffect(() => {
     (async () => {
       try {
         const signer = await walletProvider.getSigner();
 
-        const primitives = new Contract(CONTRACT_ADDRESS, abi, defaultProvider);
-
-        const primitivesWithSigner = new Contract(
-          CONTRACT_ADDRESS,
-          abi,
-          signer
+        const provider = new ethers.JsonRpcProvider(
+          "https://polygon-mumbai.g.alchemy.com/v2/D2tYfFd1wVX7OQIYW11wx74GyYUGSNpf"
         );
 
-        // const deposits = await primitives.myOrders(user.address);
-        const result: Orders = [];
-        (async () => {
-          for (let i = 0; i <= 1000; i++) {
-            const deposit = await primitives.orders(i);
-            console.log({ ...deposit });
-            if (deposit[0] === "0x3B66e7a2C8147EA2f5FCf39613492774F361A0DF") {
-              result.push({ ...deposit });
-            }
-          }
-        })();
-        setOrders(result);
+        const nfpmContract = new Contract(
+          INONFUNGIBLE_POSITION_MANAGER,
+          managerAbi,
+          provider
+        );
 
+        const numPositions = await nfpmContract.balanceOf(
+          "0xAFF32170DA4c3E31A035ca6299D07504Af4E0BA5"
+        );
+
+        console.log(numPositions);
+
+        const calls = [];
+
+        for (let i = 0; i < numPositions; i++) {
+          calls.push(
+            nfpmContract.tokenOfOwnerByIndex(
+              "0xAFF32170DA4c3E31A035ca6299D07504Af4E0BA5",
+              i
+            )
+          );
+        }
+
+        const positionIds = await Promise.all(calls);
+
+        setId(positionIds);
+
+        const positionCalls = [];
+
+        for (let id of positionIds) {
+          positionCalls.push(nfpmContract.positions(id));
+        }
+
+        const callResponses = await Promise.all(positionCalls);
+
+        console.log(callResponses);
+
+        const positionInfos = callResponses.map((position) => {
+          return {
+            token0: position.token0,
+            token1: position.token1,
+            tickLower: position.tickLower,
+            tickUpper: position.tickUpper,
+            liquidity: position.liquidity,
+            fee: position.fee,
+          };
+        });
+        console.log(positionInfos);
+        setPosition(positionInfos);
+        console.log(position);
       } catch (error) {
         console.log(error);
       }
     })();
   }, []);
 
-  const handleChange =
-    (panel: string) => (e: React.SyntheticEvent, isExpanded: boolean) => {
-      e.preventDefault();
-      setExpanded(isExpanded ? panel : false);
+  const deleteOrder =async (tokenID, token0, token1, fee) => {
+    try{
+    const signer = await walletProvider.getSigner();
+    const primitivesWithSigner = new Contract(CONTRACT_ADDRESS, abi, signer);
+    const tx = await primitivesWithSigner.decreaseLiquidity(
+      tokenID, token0, token1, fee
+    );
+    tx.wait();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      console.log(orders);
-    };
-
+  if (position.length > 0) {
   return (
     <div {...stylex.props(styles.wrapper)}>
-      {orders[0] ? (
-        orders.map((el, i) => (
-          <Accordion
-            expanded={expanded === `order${i}`}
-            onChange={handleChange(`order${i}`)}
-            key={i}
-            sx={{
-              background: "transparent",
-              border: "1px solid #f8f8f8",
-              color: "#f8f8f8",
-            }}
+      {position.map((e, index) => ( 
+        <Accordion
+          key={index}
+          sx={{
+            background: "transparent",
+            border: "1px solid #f8f8f8",
+            color: "#f8f8f8",
+          }}
+          {...stylex.props(styles.text)}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "#f8f8f8" }} />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
           >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon sx={{ color: "#f8f8f8" }} />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <p>{el[0]}</p>
-            </AccordionSummary>
-            <AccordionDetails>1</AccordionDetails>
-          </Accordion>
-        ))
-      ) : (
-        <>No</>
-      )}
+            <p> Position ID: {Number(iD[index])} </p>
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <p>Token1: {e.token0}</p>
+            <p>Token2: {e.token1}</p>
+            <p>UpperTick: {Number(e.tickUpper)}</p>
+            <p>LowerTick: {Number(e.tickLower)}</p>
+            <p>Liquidity: {Number(e.liquidity)}</p>
+            <Button sx={{ mt: "40px" }} variant="outlined" onClick={() => deleteOrder(iD[index], e.token0, e.token1, e.fee)}>
+              Close Position
+            </Button>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
-  );
+  );} else { return (
+    <div>
+      <h1>No tasks</h1>
+    </div>
+  )
+}
 };
 
 export default OrdersList;
